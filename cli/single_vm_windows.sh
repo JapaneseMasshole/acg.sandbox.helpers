3d680629f6e8b7ec1d47e4ba0ae597b84ff0a8f9
JapaneseMasshole <me@japanesemasshole.com>
2022-07-22 05:55:48 +0000

    completed whole process. - single_vm_windows.sh -> Creates vnet, nic, nsg, pub ip and vm - single_wm_windows_destroy.sh -> Deletes all resources except storage account

--- cli/single_vm_windows.sh
+++ cli/single_vm_windows.sh
@@ -6,6 +6,9 @@ nic="nic1"
 vnetname="vnet1"
 subnetname="subnet1"
 pubip="pip-win-vm1"
+vmname="vm01"
+imagename="Win2019Datacenter"
+sizename="Standard_B1s"
 
 echo "resource group name is $rg"
 echo "default location is $loc"
@@ -16,12 +19,13 @@ echo "Creating Network Security Group '$nsg'..."
 az network nsg create -g $rg -l $loc -n $nsg
 
 echo "Creating public ip '$pubip'..."
-az network public-ip create -g $rg -l $loc -n $pubip
+az network public-ip create -g $rg -l $loc -n $pubip --sku Standard
 
 echo "Creating virtual network '$vnetname' & subnet '$subnetname'..."
 az network vnet create -g $rg -l $loc -n $vnetname --subnet-name $subnetname --nsg $nsg
 
 echo "Creating nic '$nic'..."
-az network nic create  -g $rg -l $loc -n $nic --public-ip-address $pubip --subnet $subnetname
+az network nic create  -g $rg -l $loc -n $nic --public-ip-address $pubip --subnet $subnetname --vnet-name $vnetname
 
-
+echo "Creating virtual machine '$vmname'..."
+az vm create -g $rg -l $loc -n $vmname --nics $nic --image $imagename --size $sizename
--- cli/single_vm_windows_destroy.sh
+++ cli/single_vm_windows_destroy.sh
@@ -4,6 +4,16 @@ echo "resource group name is $rg"
 echo "default location is $loc"
 vnet=$(az network vnet list --query [].name -o tsv)
 
+echo "Deleting all virtual machines..."
+az vm delete --ids $(az vm list -g $rg --query "[].id" -o tsv)
+
+echo "Deleting all disks..."
+az disk delete --ids $(az disk list -g $rg --query "[].id" -o tsv)
+
+nic=$(az network nic list --query [].name -o tsv)
+echo "Deleting nic $nic..."
+az resource delete -g $rg -n $nic --resource-type "Microsoft.Network/networkInterfaces"
+
 echo "Deleting virtual network $vnet and its subnets..."
 az resource delete -g $rg -n $vnet --resource-type "Microsoft.Network/virtualNetworks"
 
