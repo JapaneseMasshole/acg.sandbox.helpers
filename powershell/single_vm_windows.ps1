5f22d19e31089d63f5af11ead0a3a9e558a73f65
JapaneseMasshole <me@japanesemasshole.com>
2022-07-23 07:56:37 +0000

    Added add/remove - virtual network - public ip address - virtual machine

--- powershell/single_vm_windows.ps1
+++ powershell/single_vm_windows.ps1
@@ -1,9 +1,9 @@
 $rg=Get-AzResourceGroup
 $nsgname="nsg1"
-$nic="nic1"
+$nicname="nic1"
 $vnetname="vnet1"
-$subnetname="subnet1"
-$pubip="pip-win-vm1"
+$subnetname="default"
+$pubipname="pip-win-vm1"
 $vmname="vm01"
 $imagename="Win2019Datacenter"
 $sizename="Standard_B1s"
@@ -13,5 +13,69 @@ echo "default location is " $rg.Location
 
 #Creating resources
 
-echo "Creating Network Security Group " $nsg
+echo "Creating Network Security Group '$nsgname'"
 $nsg = New-AzNetworkSecurityGroup -Name $nsgname -ResourceGroupName $rg.ResourceGroupName  -Location  $rg.Location
+
+echo "Creating Public IP Address '$pubipname'"
+$pubipparams = @{
+    Name = $pubipname
+    Location = $rg.Location
+    ResourceGroupName = $rg.ResourceGroupName
+    AllocationMethod = "Static"
+}
+
+$pubip = New-AzPublicIPAddress @pubipparams
+
+echo "Creating Virtual Network '$vnetname'"
+$vnet = @{
+    Name = $vnetname
+    ResourceGroupName = $rg.ResourceGroupName
+    Location = $rg.Location
+    AddressPrefix = '10.0.0.0/16'    
+}
+$virtualNetwork = New-AzVirtualNetwork @vnet
+
+echo "Configuration subnet"
+$subnet = @{
+    Name = $subnetname
+    VirtualNetwork = $virtualNetwork
+    AddressPrefix = '10.0.0.0/24'
+    NetworkSecurityGroup = $nsg
+}
+$subnetConfig = Add-AzVirtualNetworkSubnetConfig @subnet
+
+echo "Updating virtual network with newly added subnet"
+$virtualNetwork | Set-AzVirtualNetwork
+
+echo "Creating a new virtual machine..."
+<#
+$vm1 = @{
+    ResourceGroupName = $rg.ResourceGroupName
+    Location = $rg.Location
+    Name = $vmname
+    VirtualNetworkName = $virtualNetwork.Name
+    SubnetName = $subnet.Name
+    DataDiskDeleteOption = 'Delete'
+    NetworkInterfaceDeleteOption = 'Delete'
+    Image = "Win2019Datacenter"
+    Size = "Standard_B1s"
+    OSDiskDeleteOption = "Delete"
+
+}
+New-AzVM @vm1 -AsJob
+#>
+
+
+
+$UserName = "User"
+$Password = ConvertTo-SecureString "P@ssword123" -AsPlainText -Force
+$psCred = New-Object System.Management.Automation.PSCredential($UserName, $Password)
+
+
+$NIC = New-AzNetworkInterface -Name $nicname -ResourceGroupName $rg.ResourceGroupName -Location $virtualNetwork.Location -SubnetId $virtualNetwork.Subnets[1].Id -PublicIpAddressId $pubip.Id
+$VirtualMachine = New-AzVMConfig -VMName $vmname -VMSize $sizename
+$VirtualMachine = Set-AzVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $vmname -Credential $psCred -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Add-AzVMNetworkInterface -VM $VirtualMachine -Id $NIC.Id
+$VirtualMachine = Set-AzVMSourceImage -VM $VirtualMachine -PublisherName 'MicrosoftWindowsServer' -Offer 'WindowsServer' -Skus '2022-datacenter-azure-edition-core' -Version latest
+New-AzVm -ResourceGroupName $rg.ResourceGroupName -Location $virtualNetwork.Location -VM $VirtualMachine
+echo "Deployment completed."
--- powershell/single_vm_windows_destroy.ps1
+++ powershell/single_vm_windows_destroy.ps1
@@ -1,5 +1,17 @@
-<<<<<<< HEAD
 $rg=Get-AzResourceGroup
 
-$nsg = Get-AzNetworkSecurityGroup
+$vm = Get-AzVM -ResourceGroupName $rg.ResourceGroupName
+echo "Deleting virtual machine '$($vm.Name)'"
+Remove-AzVM -ResourceGroupName $rg.ResourceGroupName -Name $vm.Name -Force
+
+$vnet = Get-AzVirtualNetwork -ResourceGroupName $rg.ResourceGroupName
+echo "Deleting virtual network '$($vnet.Name)'"
+Remove-AzVirtualNetwork -Name $vnet.Name -ResourceGroupName $rg.ResourceGroupName -Force
+
+$nsg = Get-AzNetworkSecurityGroup -ResourceGroupName $rg.ResourceGroupName
+echo "Deleting network security group '$($nsg.Name)'"
 Remove-AzNetworkSecurityGroup -Name $nsg.Name -ResourceGroupName $rg.ResourceGroupName -Force
+
+$pubip = Get-AzPublicIpAddress -ResourceGroupName $rg.ResourceGroupName
+echo "Deleting public ip address '$($pubip.Name)'"
+Remove-AzPublicIpAddress -Name $pubip.Name -ResourceGroupName $rg.ResourceGroupName -Force
