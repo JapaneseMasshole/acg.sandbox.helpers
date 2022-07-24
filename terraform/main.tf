8ac222cb05d18a0b0bebc9859d15bd74ef337464
Jimmy Hattori <JapaneseMasshole@users.noreply.github.com>
2022-07-24 15:28:37 +0900

    Added vnet & ignoring tfstate files

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,24 @@ data "azurerm_storage_account" "storageacct" {
     name = var.storage_account_name
     resource_group_name = data.azurerm_resource_group.rg.name
 }
+
+resource "azurerm_virtual_network" "vnet" {
+    name = "VNET01"
+    address_space = ["10.0.0.0/16"]
+    location = data.azurerm_resource_group.rg.location
+    resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet01" {
+    name = "subnet01"
+    address_prefixes = ["10.0.1.0/24"]
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet02" {
+    name = "subnet02"
+    address_prefixes = ["10.0.2.0/24"]
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    resource_group_name = data.azurerm_resource_group.rg.name
+}
