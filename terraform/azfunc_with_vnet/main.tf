07489e6b5db36ea10e68ff6be929ce798738696d
Jimmy Hattori <me@japanesemasshole.com>
2023-05-13 22:57:21 +0900

    Added key vault

--- terraform/azfunc_with_vnet/main.tf
+++ terraform/azfunc_with_vnet/main.tf
@@ -11,9 +11,16 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+        key_vault {
+          purge_soft_delete_on_destroy    = true
+          recover_soft_deleted_key_vaults = true
+        }
+      }
   skip_provider_registration = "true"
 }
+
+data "azurerm_client_config" "current" {}
 
 data "azurerm_resource_group" "rg" {
     name = var.resource_group_name
@@ -29,6 +36,35 @@ data "azurerm_storage_account" "storageacct" {
     resource_group_name = data.azurerm_resource_group.rg.name
 }
 
+resource "azurerm_key_vault" "example" {
+  name                        = "kv-motoki-06"
+  location                    = data.azurerm_resource_group.rg.location
+  resource_group_name         = data.azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
 resource "azurerm_virtual_network" "vnet" {
     name = "VNET01"
     address_space = ["10.0.0.0/16"]
@@ -41,6 +77,7 @@ resource "azurerm_subnet" "subnet01" {
     address_prefixes = ["10.0.1.0/24"]
     virtual_network_name = azurerm_virtual_network.vnet.name
     resource_group_name = data.azurerm_resource_group.rg.name
+    service_endpoints = ["Microsoft.KeyVault"]
     delegation {
       name = "mtk-delegation-01"
 
@@ -50,6 +87,7 @@ resource "azurerm_subnet" "subnet01" {
       }
     }
 }
+
 
 /*
 resource "azurerm_network_security_group" "nsg" {
